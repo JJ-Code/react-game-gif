250653eb4919761e0784fbdf3f8b05e096838e92
Jaymee Liu <{jaymeeliu@gmail.com}>
2019-08-24 16:25:25 -0700

    updates

--- src/Game.js
+++ src/Game.js
@@ -1,10 +1,46 @@
 import React, { Component } from "react";
 import "./App.css";
+import axios from "axios"
 
 
 class Game extends Component {
+constructor(props){
+    super(props);
+    this.state = {
+
+    }
 
 
+
+
+
+    
+
+}
+
+    // const url = "https://api.giphy.com/v1/gifs/search?q="
+    // const inputField = document.querySelector("#giphy-input");
+    // const apiKey = "&api_key=39a3e436bae449eebf5904e0af9ad67c&limit=8";
+
+
+
+
+  performSearch = (props = "example") => {
+    //console.log(this.props)
+    let wordSearch = this.props.word
+    axios.get(`https://www.dictionaryapi.com/api/v3/references/collegiate/json/${wordSearch}?key=84b98cb2-2053-4c97-9062-34a7f5632399`).then(response => {
+      //console.log(response);
+      console.log(response.data[0].shortdef[0]);
+      return this.setState({
+        clueResults: response.data[0].shortdef[0]
+      });
+
+    }).catch(err => {
+      console.log('Error happened during fetching!', err);
+    });
+
+    console.log("clicked clicked BYE");
+  };
 
     render() {
 
